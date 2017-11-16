a059e2de3e8c4f634c0fbadd0d66dabc4ced260e
David Valentino <davidvalen95@gmail.com>
2017-11-16 13:26:43 +0700

    ready to use

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -10,7 +10,27 @@ import {BaseForm} from '../../forms/base-form';
 export class LoginComponent implements OnInit {
 
   public baseForms: BaseForm[] = [];
-  c
+  constructor(private title: Title) {
+    title.setTitle('Login');
+
+    const usernameForm: BaseForm = new BaseForm('Username', 'Username', 'username', 'text');
+    usernameForm.rules = {
+      'required': true,
+      'minlength': 5,
+      'maxlength': 7,
+      'pattern': '[\\w]*',
+      'patternInfo': 'Only word and number no spaces and symbols'
+    };
+    usernameForm.value = 'myName';
+
+    const passwordForm: BaseForm = new BaseForm('Password', 'Password', 'password', 'password');
+    passwordForm.rules = {
+      'required': true,
+      'maxlength': 10
+    };
+    this.baseForms.push(usernameForm, passwordForm);
+
+  }
 
   ngOnInit() {
   }
